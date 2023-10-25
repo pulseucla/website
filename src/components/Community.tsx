662121b89dec971967e32cfd55c9c081bfe4cbd4
lawtlee <lawsac2947@gmail.com>
2023-10-25 09:41:12 -0700

    Lint Errors

--- src/components/Community.tsx
+++ src/components/Community.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Navbar from "./Navbar";
 import Footer from "./footer";
 import { Link } from "react-router-dom";
